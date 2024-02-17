03fcd986f90f97178b489bf01cf59f1d441cc1cd
Paul Momo <mpny98@yahoo.com>
2024-02-17 04:29:19 -0500

    melly le reve americain

---
+++ melly.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Description: Variables in scripting
+# Author: Paul Momo
+# Date: 2/17/2024
+
+FIRST_NAME=princeton
+LAST_NAME=Jukane-Momo
+AGE=20
+COLOR=PURPLE
+
+echo "${FIRST_NAME} is a very hardworking girl"
+
+echo "Her last name is ${LAST_NAME} and her favorite color is ${COLOR}"
+echo "${FIRST_NAME} is ${AGE} years old"
