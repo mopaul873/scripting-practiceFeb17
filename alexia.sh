076173f5c7dc9579781b7c4812aec4067d602fd7
Paul Momo <mpny98@yahoo.com>
2024-02-17 04:27:21 -0500

    Alexia warriors

---
+++ alexia.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Description
+# Author:
+# Date:
+
+
+NAME=Kate
+
+echo " $NAME is a wonderful future wife"
+echo " I will complete all the requirements with $NAME family by next year"
+
+echo " I will marry $NAME in 2025 in a very nice deserving wedding to her"
+
+echo " I am still hopeful, alive and ambitious because of $NAME"
+
+
+
